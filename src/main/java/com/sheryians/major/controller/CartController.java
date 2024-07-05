d1b272e4794c8c9c048dd2ecefdd8f598a7aaf86
Ajay <yadavaj9527@gmail.com>
2024-07-05 08:58:25 +0530

    Add and Delete feature

--- src/main/java/com/sheryians/major/controller/CartController.java
+++ src/main/java/com/sheryians/major/controller/CartController.java
@@ -1,44 +1,62 @@
 package com.sheryians.major.controller;
 
+import com.sheryians.major.global.GlobalData;
+import com.sheryians.major.model.Product;
+import com.sheryians.major.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
-import com.sheryians.major.global.GlobalData;
-import com.sheryians.major.model.Product;
-import com.sheryians.major.service.ProductService;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class CartController {
 
 	@Autowired
 	ProductService productService;
-	
+
 	@GetMapping("/addToCart/{id}")
 	public String addToCart(@PathVariable int id) {
 		GlobalData.cart.add(productService.getProductById(id).get());
 		return "redirect:/shop";
 	}
-	
-	
-	@GetMapping("/cart")
-	public String getCart(Model model) {
-		model.addAttribute("cartCount", GlobalData.cart.size());
-		model.addAttribute("total", GlobalData.cart.stream().mapToDouble(Product::getPrice).sum());
-		model.addAttribute("cart", GlobalData.cart);
-		return "cart";
-	}
-	
-	
-	@GetMapping("/cart/removeItem/{index}")
-	public String removeCart(@PathVariable int index) {
-		GlobalData.cart.remove(index);
+
+
+	@GetMapping("/addToCartFromCart/{id}")
+	public String addToCartFromCart(@PathVariable int id) {
+		GlobalData.cart.add(productService.getProductById(id).get());
 		return "redirect:/cart";
 	}
-	
+
+	@GetMapping("/cart")
+	public String getCart(Model model) {
+		Map<Product, Long> productCountMap = GlobalData.cart.stream()
+				.collect(Collectors.groupingBy(product -> product, Collectors.counting()));
+
+		model.addAttribute("cartCount", GlobalData.cart.size());
+		model.addAttribute("total", GlobalData.cart.stream().mapToDouble(Product::getPrice).sum());
+		model.addAttribute("productCountMap", productCountMap);
+		return "cart";
+	}
+
+	@GetMapping("/cart/removeItem/{id}")
+	public String removeCart(@PathVariable int id) {
+		Product productToRemove = null;
+		for (Product product : GlobalData.cart) {
+			if (product.getId() == id) {
+				productToRemove = product;
+				break;
+			}
+		}
+		if (productToRemove != null) {
+			GlobalData.cart.remove(productToRemove);
+		}
+		return "redirect:/cart";
+	}
+
 	@GetMapping("/checkout")
 	public String checkoutCart(Model model) {
 		model.addAttribute("total", GlobalData.cart.stream().mapToDouble(Product::getPrice).sum());
--- src/main/java/com/sheryians/major/model/Product.java
+++ src/main/java/com/sheryians/major/model/Product.java
@@ -1,14 +1,7 @@
 package com.sheryians.major.model;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-import lombok.Data;
+import javax.persistence.*;
+import java.util.Objects;
 
 
 @Entity
@@ -33,13 +26,25 @@ public class Product {
 	private String description;
 
 	private String imageName;
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Product product = (Product) o;
+		return id == product.id;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 	
 	
 	
